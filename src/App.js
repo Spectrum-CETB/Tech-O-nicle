350a1bb827fc8492c26753d0e37c70a295f8235a
Rupesh Chandra Mohanty <rupeshmohanty67@gmail.com>
2021-10-15 21:32:50 +0530

    PascalCase error fixed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Feed from "./Components/Feed/Feed";
-import Specific_blog from "./Components/Specific Blog/Specific_blog";
+import SpecificBlog from "./Components/Specific Blog/Specific_blog";
 
 
 function App() {
   return (
     <div className="App">
       <Feed/>
-      <Specific_blog/>
+      <SpecificBlog/>
     </div>
   );
 }

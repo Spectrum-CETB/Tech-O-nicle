90fdf5ecfcff16563d190498c2cc11d35292bd82
jawed321 <jaweda162@gmail.com>
2021-10-15 22:59:30 +0530

    Update Feed.js

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -14,23 +14,23 @@ import CardButton from './CardButton';
 function Feed() {
     const responsive = {
         superLargeDesktop: {
-          // the naming can be any, depends on you.
-          breakpoint: { max: 4000, min: 1200 },
-          items: 4
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 1200 },
+            items: 4
         },
         desktop: {
-          breakpoint: { max: 1200, min: 768 },
-          items: 3
+            breakpoint: { max: 1200, min: 768 },
+            items: 3
         },
         tablet: {
-          breakpoint: { max: 768, min: 550 },
-          items: 2
+            breakpoint: { max: 768, min: 550 },
+            items: 2
         },
         mobile: {
-          breakpoint: { max: 550, min: 0 },
-          items: 1
+            breakpoint: { max: 550, min: 0 },
+            items: 1
         }
-      };
+    };
     return (
         <>
             <div className="container-fluid maincontainer">
@@ -51,8 +51,8 @@ function Feed() {
                             <div className="card-body">
                                 <h5 className="card-title">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h5>
                                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <p style={{display:'list-item',listStyleType:'disc',listStylePosition:'inside'}}><span className="liststyle">4 min read</span></p>
-                                <CardButton/>
+                                <p style={{ display: 'list-item', listStyleType: 'disc', listStylePosition: 'inside' }}><span className="liststyle">4 min read</span></p>
+                                <CardButton />
                             </div>
                         </div>
                     </div>
@@ -62,8 +62,8 @@ function Feed() {
                             <div className="card-body">
                                 <h5 className="card-title">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h5>
                                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <p style={{display:'list-item',listStyleType:'disc',listStylePosition:'inside'}}><span className="liststyle">4 min read</span></p>
-                                <CardButton/>
+                                <p style={{ display: 'list-item', listStyleType: 'disc', listStylePosition: 'inside' }}><span className="liststyle">4 min read</span></p>
+                                <CardButton />
                             </div>
                         </div>
                     </div>
@@ -72,60 +72,62 @@ function Feed() {
                             <div className="content21 mb-3">
                                 <h5 className="mb-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h5>
                                 <p className="mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit veritatis necessitatibus.</p>
-                                <p style={{display:'list-item',listStyleType:'disc',listStylePosition:'inside'}}><span className="liststyle">4 min read</span></p>
+                                <p style={{ display: 'list-item', listStyleType: 'disc', listStylePosition: 'inside' }}><span className="liststyle">4 min read</span></p>
                             </div>
                             <div className="content21 mt-3">
                                 <h5 className="mb-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h5>
                                 <p className="mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit veritatis necessitatibus.</p>
-                                <p style={{display:'list-item',listStyleType:'disc',listStylePosition:'inside'}}><span className="liststyle">4 min read</span></p>
+                                <p style={{ display: 'list-item', listStyleType: 'disc', listStylePosition: 'inside' }}><span className="liststyle">4 min read</span></p>
                             </div>
                         </div>
                     </div>
                     <div className="content2767 mt-4 mb-4" style={{ background: '#388087' }}>
                         <div className="content21 mt-2 mb-2">
-                  
+
                             <h5 className="mb-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h5>
 
-                            <p style={{display:'list-item',listStyleType:'disc',listStylePosition:'inside'}}><span className="liststyle">4 min read</span></p>
+                            <p style={{ display: 'list-item', listStyleType: 'disc', listStylePosition: 'inside' }}><span className="liststyle">4 min read</span></p>
                         </div>
                         <div className="content21">
                             <h5 className="mb-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h5>
 
-                            <p style={{display:'list-item',listStyleType:'disc',listStylePosition:'inside'}}><span className="liststyle">4 min read</span></p>
+                            <p style={{ display: 'list-item', listStyleType: 'disc', listStylePosition: 'inside' }}><span className="liststyle">4 min read</span></p>
                         </div>
                     </div>
                     <div className="content2g767 mt-4 mb-4" style={{ background: '#388087' }}>
                         <div className="content21 mt-3 mb-3">
                             <h5 className="mb-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h5>
 
-                            <p style={{display:'list-item',listStyleType:'disc',listStylePosition:'inside'}}><span className="liststyle">4 min read</span></p>
+                            <p style={{ display: 'list-item', listStyleType: 'disc', listStylePosition: 'inside' }}><span className="liststyle">4 min read</span></p>
                         </div>
                         <div className="content21">
                             <h5 className="mb-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h5>
 
-                            <p style={{display:'list-item',listStyleType:'disc',listStylePosition:'inside'}}><span className="liststyle">4 min read</span></p>
+                            <p style={{ display: 'list-item', listStyleType: 'disc', listStylePosition: 'inside' }}><span className="liststyle">4 min read</span></p>
                         </div>
                         <div className="content21">
                             <h5 className="mb-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h5>
 
-                            <p style={{display:'list-item',listStyleType:'disc',listStylePosition:'inside'}}><span className="liststyle">4 min read</span></p>
+                            <p style={{ display: 'list-item', listStyleType: 'disc', listStylePosition: 'inside' }}><span className="liststyle">4 min read</span></p>
                         </div>
                     </div>
                 </div>
-                <Carousel responsive={responsive}
-                infinite={true}
-                autoPlay={ true }
-                autoPlaySpeed={10000}
-                transitionDuration={5000}
-                swipeable={true}
-                showDots={false}
-                 className="mb-4">
-                    <Slidecard image={image4}/>
-                    <Slidecard image={image1}/>
-                    <Slidecard image={image2}/>
-                    <Slidecard image={image4}/>
-                    <Slidecard image={image3}/>
-                    <Slidecard image={image}/>
+                <Carousel 
+                    autoPlay={true}
+                    infinite={true}
+                    centerMode={false}
+                    arrows
+                    slidesToSlide={1}
+                    //itemClass="image-item"
+                    responsive={responsive}
+                    //removeArrowOnDeviceType={["tablet", "mobile"]}
+                    className="mb-4">
+                    <Slidecard image={image4} />
+                    <Slidecard image={image1} />
+                    <Slidecard image={image2} />
+                    <Slidecard image={image4} />
+                    <Slidecard image={image3} />
+                    <Slidecard image={image} />
                 </Carousel>
             </div>
         </>

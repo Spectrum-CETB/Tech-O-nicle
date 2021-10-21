d15addf89da8bc5091a8746600a7852c3262fd28
Rupesh Chandra Mohanty <rupeshmohanty67@gmail.com>
2021-10-21 18:44:58 +0530

    Routing done in app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+// importing react router dom!
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import CreateBlog from "./Components/Create Blog/CreateBlog";
 import Feed from "./Components/Feed/Feed";
 import SpecificBlog from "./Components/Specific Blog/Specific_blog";
 
-
 function App() {
   return (
     <div className="App">
-      <Feed/>
-      <SpecificBlog/>
-      <CreateBlog/>
+      <Router>
+        <Route path = "/dashboard">
+          <Feed/>
+        </Route>
+        <Route path = "/blog/:id">
+          <SpecificBlog/>
+        </Route>
+        <Route path = "/create-blog">
+          <CreateBlog/>
+        </Route>
+      </Router>
     </div>
   );
 }

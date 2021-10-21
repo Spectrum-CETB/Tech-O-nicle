b8592939310cec8e9d96017807cc462fce8a5e4d
Shruti Mohanty <shrutimohanty2019@gmail.com>
2021-10-21 21:38:53 +0530

    login signup section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import CreateBlog from "./Components/Create Blog/CreateBlog";
 import Feed from "./Components/Feed/Feed";
 import SpecificBlog from "./Components/Specific Blog/Specific_blog";
+import LoginSignup from "./Components/Login Signup/LoginSignup"
 
 function App() {
   return (
@@ -17,6 +18,9 @@ function App() {
         </Route>
         <Route path = "/create-blog">
           <CreateBlog/>
+        </Route>
+        <Route path = "/login-signup">
+          <LoginSignup/>
         </Route>
       </Router>
     </div>
---
+++ src/Components/Login Signup/LoginSignup.js
@@ -0,0 +1,75 @@
+import React, {useState} from 'react'
+import './Style.css'
+
+
+
+function LoginSignup() {
+    const [rightPanel, setRightPanel] = useState(false);
+    function handleSignin(){
+        setRightPanel(true);
+    }
+    function handleSignup(){
+        setRightPanel(false);
+    }
+
+        
+    return (
+        <div className="container__1 ">
+        <div className={rightPanel ? "container container_3 right-panel-active" : "container container_3"} id="container_3">
+       
+        <div className="form-container sign-up-container">
+            <form action="#">
+                <h1>Sign in</h1>
+                <div className="social-container">
+                    <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
+                    <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
+                    <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
+                </div>
+                <span>or use your account</span>
+                <input type="email" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <a href="#">Forgot your password?</a>
+                <button>Sign In</button>
+            </form>
+        </div>
+        <div className="form-container sign-in-container">
+            <form action="#">
+                <h1>Create Account</h1>
+                <div className="social-container">
+                    <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
+                    <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
+                    <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
+                </div>
+                <span>or use your email for registration</span>
+                <input type="text" placeholder="Name" />
+                <input type="email" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <button>Sign Up</button>
+            </form>
+        </div>
+        <div className="overlay-container">
+            <div className="overlay">
+
+                <div className="overlay-panel overlay-right">
+                    <h1>Welcome!</h1>
+                    <p>Enter your personal details and start your journey on TECHONICLE with us</p>
+                    <p>Already have an account?</p>
+                    <button className="ghost signIn" id="signIn"  onClick={handleSignin}>Sign In</button>
+                </div>
+                
+                <div className="overlay-panel overlay-left">
+                    <h1>Welcome Back!</h1>
+                    <p>To explore the world of TECHONICLE with us please login with your personal info</p>
+                    <p>Not a member yet?</p>
+                    <button className="ghost signUp" id="signUp" onClick={handleSignup}>Sign Up</button>
+                </div>
+
+                
+            </div>
+        </div>
+    </div>
+    </div>
+    )
+}
+
+export default LoginSignup

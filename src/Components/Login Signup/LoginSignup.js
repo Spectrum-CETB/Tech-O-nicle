cafa128561e704be71e06ec425a3df0813324ee1
Shruti Mohanty <shrutimohanty2019@gmail.com>
2021-10-29 02:02:11 +0530

    minor changes

--- src/Components/Login Signup/LoginSignup.js
+++ src/Components/Login Signup/LoginSignup.js
@@ -19,7 +19,7 @@ function LoginSignup() {
        
         <div className="form-container sign-up-container">
             <form action="#">
-                <h1>Sign in</h1>
+                <h1 className="heading">Sign in</h1>
                 <div className="social-container">
                     <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
                     <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
@@ -28,13 +28,13 @@ function LoginSignup() {
                 <span className="spn">or use your account</span>
                 <input type="email" placeholder="Email" />
                 <input type="password" placeholder="Password" />
-                <a href="#">Forgot your password?</a>
+                <a href="#" className="social">Forgot your password?</a>
                 <button>Sign In</button>
             </form>
         </div>
         <div className="form-container sign-in-container">
             <form action="#">
-                <h1>Create Account</h1>
+                <h1 className="heading">Create Account</h1>
                 <div className="social-container">
                     <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
                     <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
@@ -52,15 +52,15 @@ function LoginSignup() {
 
                 <div className="overlay-panel overlay-right">
                     <h1>Welcome!</h1>
-                    <p>Enter your personal details and start your journey on TECHONICLE with us</p>
-                    <p>Already have an account?</p>
+                    <p className="para">Enter your personal details and start your journey on TECHONICLE with us</p>
+                    <p className="para">Already have an account?</p>
                     <button className="ghost signIn" id="signIn"  onClick={handleSignin}>Sign In</button>
                 </div>
                 
                 <div className="overlay-panel overlay-left">
                     <h1>Welcome Back!</h1>
-                    <p>To explore the world of TECHONICLE with us please login with your personal info</p>
-                    <p>Not a member yet?</p>
+                    <p className="para">To explore the world of TECHONICLE with us please login with your personal info</p>
+                    <p className="para">Not a member yet?</p>
                     <button className="ghost signUp" id="signUp" onClick={handleSignup}>Sign Up</button>
                 </div>
 
